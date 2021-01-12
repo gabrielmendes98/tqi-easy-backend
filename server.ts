b9350606e0499fa2749b386167212670737b0569
Gabriel Mendes de Souza Santiago <gabriel.santiago@tqi.com.br>
2021-01-12 14:55:15 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,28 @@
+import * as jsonServer from 'json-server';
+import * as fs from 'fs';
+import * as https from 'https';
+
+import { handleAuthentication } from './auth';
+import { handleAuthorization } from './authz';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.post('/login', handleAuthentication);
+server.use('/orders', handleAuthorization);
+
+server.use(router);
+
+const options = {
+  cert: fs.readFileSync('./keys/cert.pem'),
+  key: fs.readFileSync('./keys/key.pem'),
+};
+
+https.createServer(options, server).listen(3333, () => {
+  console.log('JSON Server running on https://localhost:3333');
+});
